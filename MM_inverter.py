d45b9fd389dc24805e6ccd296e78f678b28054c9
jdlivingston <47476464+jdlivingston@users.noreply.github.com>
2019-10-17 16:35:25 +1100

    Update MM_inverter.py
    Added argpars and new Pool mpi

--- MM_inverter.py
+++ MM_inverter.py
@@ -10,63 +10,7 @@ from multiprocessing import Pool
 # Modified from RC polarimetry script from 10 August 2016 and N. McClure-Griffiths 10 Dec 2018
 # WORKS FOR PYTHON 3
 
-def inputs(argv):
-    '''
-    Takes inputs from command line. 
-            Inputs:
-            -f <freq> = centre frequency in MHz
-            -s <source> = source name in RA-DEC convention from miriad
-            -1 <start_chan> = starting channel number
-            -2 <end_chan> = final channel number
-            -d <step_size> = channel step_size for images
-            -b <field_size> = size of field in pixels 
-            -n <core_num> = number of cores to run task on
-    '''
-    freq = ''
-    mfs = False
-    clean = False
-    try:
-        opts, args = getopt.getopt(argv,"hcf:s:1:2:d:b:n:",["freq=","source=","start_chan=",
-                                                          "end_chan=","step_size=","field_size="
-                                                          ,"core_num="])
-        print(opts,args)
-    except getopt.GetoptError:
-        print('input error, check format')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('''
-            Takes inputs from command line. 
-            Inputs:
-            -f <freq> = centre frequency in MHz
-            -s <source> = source name in RA-DEC convention from miriad
-            -1 <start_chan> = starting channel number
-            -2 <end_chan> = final channel number
-            -d <step_size> = channel step_size for images
-            -b <field_size> = size of field in pixels 
-            -n <core_num> = number of cores to run task on
-            ''')
-            sys.exit()
-        elif opt in ("-f", "--freq"):
-            freq = arg
-        elif opt in ("-1", "--start_chan"):
-            schan = int(arg)
-        elif opt in ("-2", "--end_chan"):
-            echan = int(arg)
-        elif opt in ("-s", "--source"):
-            source = arg
-        elif opt in ("-d", "--step_size"):
-            step = int(arg)
-        elif opt in ("-b", "--field_size"):
-            field = int(arg)
-        elif opt in ("-n", "--core_num"):
-            core = int(arg)
-    return freq,source,schan,echan,step,field,core
-
-# Get the inputs before calling next function
-freq,source,schan,echan,step,field,core=inputs(sys.argv[1:])
-
-def grid_images(t):
+def grid_images(args):
     '''
     Takes inputs and runs miriad invert from command line producing dirty maps and beams
     
@@ -79,6 +23,7 @@ def grid_images(t):
     Outputs:
     for each channel band produces dirty maps (.map) for each Stokes parameter and beam images
     '''
+    chan, step, source, freq, field = args
     #Get all uvaver files
     uvaver_locations=glob.glob(f'../../*/*/{source}*.uvaver') #!only works for where my files are stored!
     var_strs=','.join(uvaver_locations)
@@ -87,30 +32,87 @@ def grid_images(t):
     print(f'Loading in {var_strs}')
     
     # Cycle over the channels which are split for multicore
-    start=schan+int((echan-schan)/core)*t
-    end=schan+int((echan-schan)/core)*(t+1)
-    
-    for chan in range(start,end,step):
-        im,qm,um,vm = [f'{source}.{freq}.{chan:04d}.{a}.map' for a in stokespars] 
-        beam = f'{source}.{freq}.{chan:04d}.beam'
-        maps=','.join([im,qm,um,vm])
+   
+    im,qm,um,vm = [f'{source}.{freq}.{chan:04d}.{a}.map' for a in stokespars] 
+    beam = f'{source}.{freq}.{chan:04d}.beam'
+    maps=','.join([im,qm,um,vm])
 
     #Check if the maps have already been created
-        if not os.path.isdir(im) and not os.path.isdir(beam):
+    if not os.path.isdir(im) and not os.path.isdir(beam):
     # Do the imaging
-            chan_str = f'chan,{step},{chan}'
-            stokes_str = ','.join(stokespars)
-            cmd = f'invert vis={var_strs} map={maps} beam={beam} line={chan_str} imsize={field},{field} cell=1,1 robust=+0.6 stokes={stokes_str} options=double,mfs'
-            print(cmd)
-            args=shlex.split(cmd)
-            p=subprocess.Popen(args, stdout=subprocess.PIPE)
+        chan_str = f'chan,{step},{chan}'
+        stokes_str = ','.join(stokespars)
+        cmd = f'invert vis={var_strs} map={maps} beam={beam} line={chan_str} imsize={field},{field} cell=1,1 robust=+0.6 stokes={stokes_str} options=double,mfs'
+        print(cmd)
+        args=shlex.split(cmd)
+        p=subprocess.Popen(args, stdout=subprocess.PIPE)
             
     # Print the output
-            for line in p.stdout:
-               print(line)
-            p.wait()
+        for line in p.stdout:
+           print(line)
+        p.wait()
             
-#Makes list of processors
-pool = Pool(processes=core)
-#Runs each chunk of freq on new processor 
-pool.map(grid_images, list(range(0,core,1)))
+def main(pool, args):
+
+    inputs = [[i, args.step_size, args.source, args.freq, args.field_size] for i in range(args.start_chan, args.end_chan, args.step_size)]
+
+    #Runs each chunk of freq on new processor
+    pool.map(grid_images, inputs)
+    pool.close()
+    print('Done')
+
+
+if __name__ == "__main__":
+    import argparse
+    import schwimmbad
+
+
+    # Help string to be shown using the -h option
+    descStr = """
+    Takes uvaver files and runs miriad invert from command line producing dirty maps and beams
+    """
+
+    # Parse the command line options
+    parser = argparse.ArgumentParser(description=descStr,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("source", metavar="source", nargs=1,
+                        type=str, help="Source name in RA-DEC convention from miriad")
+
+    parser.add_argument("-f", dest="freq", type=int, default=2100,
+                        help="centre frequency in MHz")
+
+    parser.add_argument("-1", dest="start_chan", type=int, default=1,
+                        help="starting channel number")
+
+    parser.add_argument("-2", dest="end_chan", type=int, default=1500,
+                        help="final channel number")
+
+    parser.add_argument("-d", dest="step_size", type=int, default=5,
+                        help="channel step_size for images")
+
+    parser.add_argument("-b", dest="field_size", type=int, default=2000,
+                        help="size of field in pixels sqr")
+
+
+
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument("--ncores", dest="n_cores", default=1,
+                       type=int, help="Number of processes (uses multiprocessing).")
+    group.add_argument("--mpi", dest="mpi", default=False,
+                       action="store_true", help="Run with MPI.")
+
+
+    args = parser.parse_args()
+    pool = schwimmbad.choose_pool(mpi=args.mpi, processes=args.n_cores)
+    #pool = schwimmbad.SerialPool()
+
+    if args.mpi:
+        if not pool.is_master():
+            pool.wait()
+            sys.exit(0)
+
+
+    # Clean the images
+    main(pool, args)
+
