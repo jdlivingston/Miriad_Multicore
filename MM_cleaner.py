e21091eecaff02ab9ce446f102c2cd95e3a04561
Jack Livingston <47476464+jdlivingston@users.noreply.github.com>
2020-02-27 15:09:28 +1100

    Update MM_cleaner.py
    Removed Miriad printing outputs and replaced with progress bar from tqdm. Also saves errors into log files.

--- MM_cleaner.py
+++ MM_cleaner.py
@@ -3,6 +3,7 @@ import subprocess,shlex,shutil
 import sys,getopt
 import glob
 from multiprocessing import Pool
+import tqdm 
 
 # Miriad Multicore Cleaner
 # Work through making images from .uvaver miriad files
@@ -25,21 +26,23 @@ def get_noise(source,freq,chan):
     noise=-100.0
     stokes='v'
     maps = f'{source}.{freq}.{chan}.{stokes}.map'
+    log_file = 'error_noise.log'
     if os.path.isdir(maps):
         fitsfile = f'{maps}.fits'
         cmd = f'fits in={maps} out={fitsfile} op=xyout '
-        print(cmd)
+        #print(cmd)
         args=shlex.split(cmd)
-        p=subprocess.Popen(args, stdout=subprocess.PIPE)
+        with open(log_file,'a') as log:
+            p=subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     # Print the output
-        for line in p.stdout:
-            print(line)
-        p.wait()
+        #for line in p.stdout:
+        #    print(line)
+            p.wait()
     # Open the FITS file
         hdul = fits.open(fitsfile)
         data = hdul[0].data
         noise=np.std(data)/2
-        print(f"RMS = {noise}")
+        #print(f"RMS = {noise}")
         os.remove(fitsfile)
     return noise
 
@@ -70,69 +73,76 @@ def clean_images(args):
         beam = f'{source}.{freq}.{chan:04d}.beam'
         rms = f'{source}.{freq}.{chan:04d}.{stokes}.rms'
         outfile = f'{source}.{freq}.{chan:04d}.{stokes}.cln.fits'
-
+        log_file = 'error_cln.log'
     # Check that the map exists before trying
         if not os.path.isdir(maps) and not os.path.isdir(beam):
-            print(f"Map {maps} does not exist")
+            #print(f"Map {maps} does not exist")
+            pass
         else:
     # Run through clean
             cmd = f'clean map={maps} beam={beam} region=percentage({region}) niters={nit} cutoff={cut_noise} out={mod}'
-            print(cmd)
+            #print(cmd)
             args=shlex.split(cmd)  # Splits the cmd into a string for subprocess
-            p=subprocess.Popen(args, stdout=subprocess.PIPE)
+            with open(log_file,'a') as log:
+                p=subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     # Print the output
-            for line in p.stdout:
-                 print(line)
-            p.wait()
+            #for line in p.stdout:
+            #     print(line)
+                p.wait()
      # Restor the images
             cmd = f'restor map={maps} beam={beam} model={mod} out={pbcorr}'
-            print(cmd)
+            #print(cmd)
             args=shlex.split(cmd)
-            p=subprocess.Popen(args, stdout=subprocess.PIPE)
+            with open(log_file,'a') as log:
+                p=subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 #         #Print the output
-            for line in p.stdout:
-                 print(line)
-            p.wait()
+            #for line in p.stdout:
+                # print(line)
+                p.wait()
 
 #         # Primary Beam Correction
             cmd = f'linmos in={pbcorr} out={cln}'
-            print(cmd)
+            #print(cmd)
             args=shlex.split(cmd)
-            p=subprocess.Popen(args, stdout=subprocess.PIPE)
+            with open(log_file,'a') as log:
+                p=subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 #         #Print the output
-            for line in p.stdout:
-                  print(line)
-            p.wait()
+            #for line in p.stdout:
+            #      print(line)
+                p.wait()
 
 #         # Copy missing RMS after primary beam correction
             cmd = f'gethd in={pbcorr}/rms log={rms}'
-            print(cmd)
+            #print(cmd)
             args=shlex.split(cmd)
-            p=subprocess.Popen(args, stdout=subprocess.PIPE)
+            with open(log_file,'a') as log:
+                p=subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 #         #Print the output
-            for line in p.stdout:
-                  print(line)
-            p.wait()
+            #for line in p.stdout:
+            #      print(line)
+                p.wait()
 
 #         # Paste missing RMS onto primary beam correction
             cmd = f'puthd in={cln}/rms value=@{rms}'
-            print(cmd)
+            #print(cmd)
             args=shlex.split(cmd)
-            p=subprocess.Popen(args, stdout=subprocess.PIPE)
+            with open(log_file,'a') as log:
+                p=subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 #         #Print the output
-            for line in p.stdout:
-                  print(line)
-            p.wait()
+            #for line in p.stdout:
+            #      print(line)
+                p.wait()
 
 #         #convert to fits
             cmd =f'fits in={cln} out={outfile} op=xyout'
-            print(cmd)
+            #print(cmd)
             args=shlex.split(cmd)  # Splits the cmd into a string for subprocess
-            p=subprocess.Popen(args, stdout=subprocess.PIPE)
+            with open(log_file,'a') as log:
+                p=subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=log)
       # Print the output
-            for line in p.stdout:
-                  print(line)
-            p.wait()
+            #for line in p.stdout:
+            #      print(line)
+                p.wait()
 
     return
 
@@ -143,9 +153,10 @@ def main(pool, args):
     inputs = [[i, args.source, args.freq, args.region, args.n_iters] for i in range(args.start_chan, args.end_chan, args.step_size)]
 
     #Runs each chunk of freq on new processor
-    pool.map(clean_images, inputs)
+    print('Cleaning Images')
+    for _ in tqdm.tqdm(pool.imap(clean_images, inputs),total=len(inputs)):
+        pass
     pool.close()
-    print('Done')
 
 
 if __name__ == "__main__":
@@ -205,18 +216,3 @@ if __name__ == "__main__":
 
     # Clean the images
     main(pool, args)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
